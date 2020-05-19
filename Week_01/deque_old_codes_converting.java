a91f3024cd51f7149419398c3d05d1449bcb73e3
刘一雄 <liuyixiong@createcloud.com>
2020-05-20 00:06:28 +0800

    updated format in deque_old_codes_converting

--- Week_01/deque_old_codes_converting.java
+++ Week_01/deque_old_codes_converting.java
@@ -1,19 +1,22 @@
+import java.util.Deque;
+import java.util.LinkedList;
 
+public class deque_old_codes_converting {
+    public static void main(final String[] args) {
+        final Deque<String> deque = new LinkedList<String>();
+        deque.addFirst("a");
+        deque.addFirst("b");
+        deque.addFirst("c");
+        System.out.println(deque);
 
-public static void public static void main(String[] args) {
-    Deque<String> deque = new LinkedList<String>();
-    deque.addFirst("a");
-    deque.addFirst("b");
-    deque.addFirst("c");
-    System.out.println(deque);
+        final String str = deque.removeFirst();
+        System.out.println(str);
+        System.out.println(deque);
     
-    String str = deque.removeFirst();
-    System.out.println(str);
-    System.out.println(deque);
-
-    while (deque.size() > 0) {
-        System.out.println(deque.removeFirst());
+        while (deque.size() > 0) {
+            System.out.println(deque.removeFirst());
+        }
+    
+        System.out.println(deque); 
     }
-
-    System.out.println(deque); 
 }
