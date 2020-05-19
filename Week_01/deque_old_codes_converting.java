842c91fbc3e6095e1bc154e57ded5f386f389832
刘一雄 <liuyixiong@createcloud.com>
2020-05-19 23:08:23 +0800

    added homework for converting to deque API

---
+++ Week_01/deque_old_codes_converting.java
@@ -0,0 +1,19 @@
+
+
+public static void public static void main(String[] args) {
+    Deque<String> deque = new LinkedList<String>();
+    deque.addFirst("a");
+    deque.addFirst("b");
+    deque.addFirst("c");
+    System.out.println(deque);
+    
+    String str = deque.removeFirst();
+    System.out.println(str);
+    System.out.println(deque);
+
+    while (deque.size() > 0) {
+        System.out.println(deque.removeFirst());
+    }
+
+    System.out.println(deque); 
+}
