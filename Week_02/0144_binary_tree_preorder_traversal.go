4c1cc4fcf2199caf745bbad1031407e2d5449ad4
刘一雄 <liuyixiong@createcloud.com>
2020-05-30 23:54:24 +0800

    added stack approach in 144 problem

--- Week_02/0144_binary_tree_preorder_traversal.go
+++ Week_02/0144_binary_tree_preorder_traversal.go
@@ -1,22 +1,47 @@
 package solutions
 
+import "container/list"
+
 // https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
 
-// recursive approach
+// stack approach
 func preorderTraversal(root *TreeNode) []int {
 	result := make([]int, 0)
 
-	var preorderTraverse func(root *TreeNode)
-	preorderTraverse = func(root *TreeNode) {
-		if root == nil {
-			return
+	l := list.New()
+
+	l.PushFront(root)
+	for l.Len() > 0 {
+		current := l.Front().Value.(*TreeNode)
+		l.Remove(l.Front())
+
+		if current == nil {
+			continue
 		}
 
-		result = append(result, root.Val)
-		preorderTraverse(root.Left)
-		preorderTraverse(root.Right)
+		result = append(result, current.Val)
+		l.PushFront(current.Right)
+		l.PushFront(current.Left)
 	}
 
-	preorderTraverse(root)
 	return result
 }
+
+// recursive approach, time O(n), space O(n)
+// func preorderTraversal(root *TreeNode) []int {
+// 	result := make([]int, 0)
+
+// 	var preorderTraverse func(root *TreeNode)
+// 	preorderTraverse = func(root *TreeNode) {
+// 		if root == nil {
+// 			return
+// 		}
+
+// 		result = append(result, root.Val)
+// 		preorderTraverse(root.Left)
+// 		preorderTraverse(root.Right)
+// 	}
+
+// 	preorderTraverse(root)
+// 	return result
+// }
